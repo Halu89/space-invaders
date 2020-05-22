163671fd49b857b708f665f0a0dd1c5d89c0c609
Corentin <corentin.briand@gmail.com>
2020-05-22 15:52:32 +0200

    initial commit

---
+++ myPaperscript.js
@@ -0,0 +1,11 @@
+var myCircle = new Path.Circle(new Point(100, 70), 50);
+myCircle.strokeColor = 'black';
+myCircle.selected = false;
+
+myCircle.removeSegment(0)
+
+myCircle.fillColor = 'black'
+
+var myRectangle = new Path.Rectangle(new Point(10,10), new Point(80, 40))
+myRectangle.selected = true;
+myRectangle.fillColor = 'yellow'
