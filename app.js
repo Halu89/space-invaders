ea556a89438770d5c89a1982cd813c0d1d6af06a
Corentin Briand <corentin.briand@gmail.com>
2020-09-05 22:40:49 +0200

    add padding to rules reduce background image size

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ function slideToggle() {
   if (slideOpen) {
     slideOpen = false;
     rulesDiv.style.height = "0px";
+    rulesDiv.style.padding = '0px';
   } else {
     slideOpen = true;
     rulesDiv.style.height = initHeight + "px";
+    rulesDiv.style.padding = '20px';
   }
 }
 
